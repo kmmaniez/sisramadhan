a5e8bf02ce35bb0f7d9b98d45ed9434aedf6f279
kmmaniez <dzlabsid@gmail.com>
2023-04-20 10:11:31 +0700

    dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,19 +19,20 @@ class DashboardController extends Controller
         // $aw = DB::select('SELECT COUNT(id_ustadh) as JUMLAHPertemuan FROM `jadwal_ajar` WHERE id_ustadh=5; ');
         // $cc = JadwalAjar::where('id_ustadh','=',5)->count();
         $countUstad = JadwalAjar::select('id_ustadh')->groupBy('id_ustadh')->get();
-        $ust = JadwalAjar::all();
-        // $ss = JadwalAjar::select('id_ustadh')->where('id_ustadh','=',3)->count();
 
         $jumlahustadh = [];
         $jumlahajar = [];
         foreach ($countUstad as $key => $value) {
+            // dump($value->ustadh->nama);
             array_push($jumlahustadh,[
-                $value->id_ustadh,
+                $value->ustadh->nama,
             ]);
             array_push($jumlahajar,[
-                static::countUstadhSchedule($value->id_ustadh)
+                static::countUstadhSchedule('tpa',$value->id_ustadh)
             ]);
         }
+        $ea = JadwalAjar::all();
+        // dump($jumlahajar,$jumlahustadh);
         // $qq = DB::select('SELECT u.nama
         // FROM `jadwal_ajar` as j JOIN ustadh as u 
         // ON u.kode_ust = j.id_ustadh
@@ -57,8 +58,16 @@ class DashboardController extends Controller
     
     public function tadarus()
     {
-        $namakelompok = Tadarus::all()->pluck('nama_kelompok');
-        return view('admin.dashboard.dashtadarus', compact('namakelompok'));
+        $namakelompok = Tadarus::all();
+        $tadarus = [
+            'jumlah' => [],
+            'listnama' => [],
+        ];
+        foreach ($namakelompok as $key => $value) {
+            array_push($tadarus['jumlah'], $value->jumlah_khatam);
+            array_push($tadarus['listnama'], $value->nama_kelompok);
+        }
+        return view('admin.dashboard.dashtadarus', compact('namakelompok','tadarus'));
     }
 
     public function getTadarus()
@@ -68,9 +77,20 @@ class DashboardController extends Controller
         ]);
     }
 
-    public function countUstadhSchedule($id)
+    public function countUstadhSchedule($table, $id)
     {
-       $result = JadwalAjar::select('id_ustadh')->where('id_ustadh','=',$id)->count();
+        switch ($table) {
+            case 'tpa':
+                $result = JadwalAjar::select('id_ustadh')->where('id_ustadh','=',$id)->count();
+                break;
+            case 'tadarus':
+                $result = JadwalAjar::select('id_ustadh')->where('id_ustadh','=',$id)->count();
+                break;
+            default:
+                # code...
+                break;
+        }
+    //    $result = JadwalAjar::select('id_ustadh')->where('id_ustadh','=',$id)->count();
        return $result;
     }
 }

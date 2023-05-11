1ad8a08340306f0104e6c39866ec601259ba0648
kmmaniez <dzlabsid@gmail.com>
2023-05-11 08:31:21 +0700

    layout public

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,6 +20,13 @@
     @yield('content')
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+	
+<div id="g_id_onload"
+     data-client_id="157261405624-vrmr3f4coa9jk8ivpn6evsk8fcg4rt8q.apps.googleusercontent.com"
+     data-context="signin"
+     data-login_uri="http://localhost:8000/login"
+     data-itp_support="true">
+</div>
 </body>
     @vite('resources/js/app.js')
 </html>

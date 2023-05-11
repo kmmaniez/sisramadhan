f1ea8c86d7f91bcc8f5ccf5e4052e51cfb0341e0
kmmaniez <dzlabsid@gmail.com>
2023-05-11 08:31:09 +0700

    layout admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Halaman @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     @vite('resources/css/app.css')
     
@@ -23,7 +23,7 @@
         width: 100%;
         min-height: 100vh;
         height: max-content;
-        padding: 0 16px;
+        padding: 0 16px 0 8px;
         display: flex;
         flex-direction: row;
         gap: 2rem;
@@ -31,11 +31,19 @@
       }
       section#ramadhan aside#nav{
         width: 324px;
-        padding-top: 40px;
+        padding-top: 48px;
         height: initial;
       }
       section#ramadhan #content{
         margin-top: 40px;
+      }
+      .nav-item.active{
+        font-weight: 700;
+        /* background-color: #eaeaea; */
+      }
+      aside ul{
+        background-color: #fff;
+        padding: 8px;
       }
     </style>
   </head>
@@ -66,51 +74,50 @@
       <!-- Cek user dengan role panitia / 1 -->
       <ul class="nav flex-column gap-2 w-100">
 
-        @if (Auth::user()->id_role == 1) 
+        {{-- @if (Auth::user()->id_role == 1)  --}}
         <li class="nav-item">
             <a class="nav-link" data-bs-toggle="collapse" href="#dashboarCollapse" role="button" aria-expanded="false" aria-controls="dashboarCollapse">Dashboard</a>
-            <ul class="child collapse border-0" id="dashboarCollapse">
-              <li class="ps-4 py-1"><a href="/admin/dashtpa" class="dropdown-item fw-bold">TPA</a></li>
-              <li class="ps-4 mt-1 py-1"><a href="/admin/dashkonsumsi" class="dropdown-item ">Kelola Konsumsi</a></li>
-              <li class="ps-4 mt-1 py-1"><a href="/admin/dashtarawih" class="dropdown-item ">Tarawih</a></li>
-              <li class="ps-4 mt-1 py-1"><a href="/admin/dashtadarus" class="dropdown-item ">Tadarus</a></li>
+            <ul class="child collapse {{ (request()->routeIs('dash*')) ? 'show' : '' }} border-0" id="dashboarCollapse">
+              <li class="ps-4 py-1"><a href="/admin/dashtpa" class="dropdown-item {{ (request()->routeIs('dash.tpa')) ? 'fw-bold' : '' }}">TPA</a></li>
+              <li class="ps-4 mt-1 py-1"><a href="/admin/dashkonsumsi" class="dropdown-item {{ (request()->routeIs('dash.konsumsi')) ? 'fw-bold' : '' }}">Kelola Konsumsi Done</a></li>
+              <li class="ps-4 mt-1 py-1"><a href="/admin/dashtarawih" class="dropdown-item {{ (request()->routeIs('dash.tarawih')) ? 'fw-bold' : '' }}">Tarawih Done</a></li>
+              <li class="ps-4 mt-1 py-1"><a href="/admin/dashtadarus" class="dropdown-item {{ (request()->routeIs('dash.tadarus')) ? 'fw-bold' : '' }}">Tadarus DONE</a></li>
             </ul>
         </li>
-        @endif
-
-        <li class="nav-item">
+        {{-- @endif --}}
+        <li class="nav-item {{ (request()->routeIs('tpa*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('tpa.index') }}">TPA</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('konsumsi*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('konsumsi.index') }}">Kelola konsumsi</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('tarawih*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('tarawih.index') }}">Tarawih</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('tadarus*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('tadarus.index') }}">Tadarus</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('khataman*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('khataman.index') }}">Khataman & Nuzulul</a>
-        </li>
-        <li class="nav-item">
+      </li>
+        <li class="nav-item {{ (request()->routeIs('zakat*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('zakat.index') }}">Zakat</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('takbiran*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('takbiran.index') }}">Takbiran</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('sholatied*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('sholatied.index') }}">Sholat Ied</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{ (request()->routeIs('warga*')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('warga.index') }}">Warga</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" data-bs-toggle="collapse" href="#laporanCollapse" role="button" aria-expanded="false" aria-controls="laporanCollapse">Laporan</a>
-          <ul class="child collapse" id="laporanCollapse">
-            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-imam" class="dropdown-item fw-bold">Laporan Imam & Bilal</a></li>
-            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-kultum" class="dropdown-item ">Laporan Pengisi Kultum</a></li>
-            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-konsumsi" class="dropdown-item ">Laporan Konsumsi</a></li>
+          <ul class="child collapse {{ (request()->routeIs('lap*')) ? 'show' : '' }} " id="laporanCollapse">
+            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-imam" class="dropdown-item {{ (request()->routeIs('lap.imam')) ? 'fw-bold' : '' }}">Laporan Imam & Bilal</a></li>
+            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-kultum" class="dropdown-item {{ (request()->routeIs('lap.kultum')) ? 'fw-bold' : '' }}">Laporan Pengisi Kultum</a></li>
+            <li class="ps-4 mt-1 py-1"><a href="/admin/laporan-konsumsi" class="dropdown-item {{ (request()->routeIs('lap.konsumsi')) ? 'fw-bold' : '' }}">Laporan Konsumsi</a></li>
           </ul>
         </li>
       </ul>
@@ -126,7 +133,8 @@
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-       
+	
+
   </body>
     @stack('script')
     @vite('resources/js/app.js')

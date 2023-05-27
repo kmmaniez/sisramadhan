95c12febfaf45cde9530301c109ba81ad2190340
kmmaniez <dzlabsid@gmail.com>
2023-05-27 08:58:32 +0700

    update public zakat

--- resources/views/zakat.blade.php
+++ resources/views/zakat.blade.php
@@ -1,10 +1,18 @@
+@php
+    $DateConv = new Hijri_GregorianConvert;
+    $format="YYYY/MM/DD";
+    $listTahun = [];
+    for ($i=0; $i < 3; $i++) { 
+        array_push($listTahun, date('Y') - $i);
+    }
+@endphp
 @extends('layouts.default')
 
 @section('content')
     <div class="container mt-5">
         <div class="title text-center">
             <h1>Distribusi Zakat</h1>
-            <h2>Tahun 2022/1443 H</h2>
+            <h2>Tahun <span id="masehi">{{ date('Y') }}</span>/<span id="hijri"><?= $DateConv->GregorianToHijri(date('Y'),'YYYY'); ?></span>H</h2>
         </div>
         <a href="/" class="btn btn-lg btn-secondary mb-4 mt-5">Kembali</a>
         <table class="table table-striped">

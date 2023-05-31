2b21241845d66948129bffa554ac0361494b5c2f
kmmaniez <dzlabsid@gmail.com>
2023-05-31 16:55:26 +0700

    update views warga

--- resources/views/admin/warga/edit.blade.php
+++ resources/views/admin/warga/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.admin')
 
 @section('content')
-    
     <div class="container">
         <div class="title text-center mb-3">
             <h1>Form Data Warga</h1>
@@ -13,104 +12,112 @@
 
             <div class="form-group mb-3">
                 <label for="nama_keluarga" class="form-label">Keluarga</label>
-                <input type="text" name="nama_keluarga" id="nama_keluarga" class="form-control" value="{{ old('nama_keluarga', $warga->nama_keluarga) }}">
+                <input type="text" name="nama_keluarga" id="nama_keluarga" class="form-control"
+                    value="{{ old('nama_keluarga', $warga->nama_keluarga) }}">
             </div>
             <div class="form-group mb-3">
                 <label for="nama_asli" class="form-label">Nama Asli</label>
-                <input type="text" name="nama_asli" id="nama_asli" class="form-control" value="{{ old('nama_asli', $warga->nama_asli) }}">
+                <input type="text" name="nama_asli" id="nama_asli" class="form-control"
+                    value="{{ old('nama_asli', $warga->nama_asli) }}">
             </div>
             <div class="form-group mb-3">
                 <label for="nama_alias" class="form-label">Nama Panggilan</label>
-                <input type="text" name="nama_alias" id="nama_alias" class="form-control" value="{{ old('nama_alias', $warga->nama_alias) }}">
+                <input type="text" name="nama_alias" id="nama_alias" class="form-control"
+                    value="{{ old('nama_alias', $warga->nama_alias) }}">
             </div>
             <div class="form-group mb-3">
-                
+
                 <div class="form mb-3">
                     <label for="alamat" class="form-label">Alamat</label>
-                    <input type="text" name="alamat" id="alamat" class="form-control" value="{{ old('alamat', $warga->alamat) }}">
+                    <input type="text" name="alamat" id="alamat" class="form-control"
+                        value="{{ old('alamat', $warga->alamat) }}">
                 </div>
-                
+
                 <div class="form d-flex justify-content-start align-items-center gap-3">
 
                     <!-- RT -->
                     <label for="rt" class="form-label">RT</label>
-                    <select class="form-select" name="rt" id="rt" style="width: 80px;" aria-label="Default select example">
+                    <select class="form-select" name="rt" id="rt" style="width: 80px;"
+                        aria-label="Default select example">
                         {{-- <option value="" selected style="display: none">0</option> --}}
                         @foreach ($listrt as $datart)
                             @if (old('rt', $warga->rt) == $datart)
                                 <option value="{{ $datart }}" selected>{{ $warga->rt }}</option>
-                            @else  
+                            @else
                                 <option value="{{ $datart }}">{{ $datart }}</option>
                             @endif
                         @endforeach
                     </select>
-                    
+
                     <!-- RW -->
                     <label for="rw" class="form-label">RW</label>
-                    <select class="form-select" name="rw" id="rw" style="width: 80px;" aria-label="Default select example">
+                    <select class="form-select" name="rw" id="rw" style="width: 80px;"
+                        aria-label="Default select example">
                         {{-- <option value="" selected style="display: none">0</option> --}}
                         @foreach ($listrw as $datarw)
                             @if (old('rt', $warga->rw) == $datarw)
                                 <option value="{{ $datarw }}" selected>{{ $warga->rw }}</option>
-                            @else  
+                            @else
                                 <option value="{{ $datarw }}">{{ $datarw }}</option>
                             @endif
                         @endforeach
                     </select>
-                    
+
                 </div>
-                
+
             </div>
             <div class="form-group mb-3">
                 <label for="nomorhp" class="form-label">Nomor HP</label>
-                <input type="tel" class="form-control" name="nomorhp" id="nomorhp" value="{{ old('nomorhp', $warga->nomor_hp) }}">
+                <input type="tel" class="form-control" name="nomorhp" id="nomorhp"
+                    value="{{ old('nomorhp', $warga->nomor_hp) }}">
             </div>
             <div class="form-group mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control" name="email" id="email" value="{{ old('email', $warga->email) }}">
+                <input type="text" class="form-control" name="email" id="email"
+                    value="{{ old('email', $warga->email) }}">
             </div>
             <div class="form d-flex justify-content-around">
                 <div class="form-group w-25 d-flex flex-column gap-2">
                     <label for="">Status</label>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="status" id="aktif" value="aktif" @if ($warga->status_keaktifan)
-                            checked
-                        @endif>
+                        <input class="form-check-input" type="radio" name="status" id="aktif" value="aktif"
+                            @if ($warga->status_keaktifan) checked @endif>
                         <label class="form-check-label" for="aktif">Aktif</label>
-                        </div>
+                    </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="status" id="nonaktif" value="nonaktif" @if (!$warga->status_keaktifan)
-                        checked
-                    @endif>
+                        <input class="form-check-input" type="radio" name="status" id="nonaktif" value="nonaktif"
+                            @if (!$warga->status_keaktifan) checked @endif>
                         <label class="form-check-label" for="nonaktif">Tidak Aktif</label>
-                    </div>                      
+                    </div>
                 </div>
                 @php
                     $datakontribusi = json_decode($warga->kontribusi);
                 @endphp
-                <div class="form-group w-75 d-flex flex-column justify-content-center flex-wrap gap-2" style="height: 140px">
+                <div class="form-group w-75 d-flex flex-column justify-content-center flex-wrap gap-2"
+                    style="height: 140px">
                     <label for="">Kontribusi</label>
 
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="check[]" value="imam" id="flexCheckDefault" {{ in_array('imam', $datakontribusi) ? 'checked' : '' }} >
-                        <label class="form-check-label" for="flexCheckDefault">Imam</label> 
-                    </div> 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="check[]" value="bilal" id="flexCheckDefault" {{ in_array('bilal', $datakontribusi) ? 'checked' : '' }}>
-                        <label class="form-check-label" for="flexCheckDefault">Bilal</label> 
+                        <input class="form-check-input" type="checkbox" name="check[]" value="imam" id="flexCheckDefault"
+                            @if (is_null($datakontribusi))  {{ '' }}
+                        @else
+                        {{ in_array('imam', $datakontribusi) ? 'checked' : '' }} @endif>
+                        <label class="form-check-label" for="flexCheckDefault">Imam</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="check[]" value="penceramah" id="flexCheckDefault" {{ in_array('penceramah', $datakontribusi) ? 'checked' : '' }}>
-                        <label class="form-check-label" for="flexCheckDefault">Penceramah</label> 
-                    </div> 
-                    {{-- <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="check[]" value="pengajartpa" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">Pengajar TPA</label> 
-                    </div> 
+                        <input class="form-check-input" type="checkbox" name="check[]" value="bilal"
+                            id="flexCheckDefault"
+                            @if (is_null($datakontribusi))  {{ '' }}
+                        @else
+                        {{ in_array('bilal', $datakontribusi) ? 'checked' : '' }} @endif>
+                        <label class="form-check-label" for="flexCheckDefault">Bilal</label>
+                    </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="check[]" value="donatur" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">Donatur</label> 
-                    </div>  --}}
+                        <input class="form-check-input" type="checkbox" name="check[]" value="penceramah" id="flexCheckDefault"
+                        @if (is_null($datakontribusi)) {{ '' }}
+                        @else {{ in_array('penceramah', $datakontribusi) ? 'checked' : '' }} @endif>
+                        <label class="form-check-label" for="flexCheckDefault">Penceramah</label>
+                    </div>
                 </div>
             </div>
 
@@ -122,5 +129,4 @@
         </form>
 
     </div>
-
 @endsection

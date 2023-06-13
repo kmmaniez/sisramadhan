a0a0463ff80c0b9feff4f27f7e1b78b45010f618
kmmaniez <dzlabsid@gmail.com>
2023-06-13 08:16:05 +0700

    update model

---
+++ app/Models/TakbiranWarga.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TakbiranWarga extends Model
+{
+    use HasFactory;
+    protected $table = 'takbiran_warga';
+}

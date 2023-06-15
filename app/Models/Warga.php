ee367f070ad444a21b01967804734543dd6cb13d
kmmaniez <dzlabsid@gmail.com>
2023-06-16 06:33:05 +0700

    update warga

--- app/Models/Warga.php
+++ app/Models/Warga.php
@@ -10,17 +10,17 @@ class Warga extends Model
     use HasFactory;
     protected $table = 'warga';
     protected $guarded = ['id'];
-
+    // protected $with = ['bukbers'];
     public $timestamps = false;
 
     // public function tadarus()
     // {
     //     return $this->hasMany(Tadarus::class,'id');
     // }
-    public function takbiran()
-    {
-        return $this->hasOne(Takbiran::class,'id');
-    }
+    // public function takbiran()
+    // {
+    //     return $this->hasOne(Takbiran::class,'id');
+    // }
     
     public function imam()
     {
@@ -36,6 +36,21 @@ class Warga extends Model
     {
         return $this->hasOne(Tarawih::class,'id');
     }
+// ------------------
+    public function takjils()
+    {
+        return $this->belongsToMany(Konsumsi::class, 'takjil_warga');
+    }
+    public function jaburs()
+    {
+        return $this->belongsToMany(Konsumsi::class, 'jabur_warga');
+    }
+    public function bukbers()
+    {
+        return $this->belongsToMany(Konsumsi::class, 'bukber_warga');
+    }
+
+
 
     public function takbirans()
     {

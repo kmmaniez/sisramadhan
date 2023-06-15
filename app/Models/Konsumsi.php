7eaa3f7bb088f64410545a2a42826885bc28ecfc
kmmaniez <dzlabsid@gmail.com>
2023-06-16 06:32:36 +0700

    update konsumsi

--- app/Models/Konsumsi.php
+++ app/Models/Konsumsi.php
@@ -10,4 +10,23 @@ class Konsumsi extends Model
     use HasFactory;
     protected $table = 'konsumsi';
     protected $guarded = ['id'];
+    protected $with = ['takjils','jaburs','bukbers'];
+
+    public function takjils()
+    {
+        return $this->belongsToMany(Warga::class, 'takjil_warga');
+    }
+    public function jaburs()
+    {
+        return $this->belongsToMany(Warga::class, 'jabur_warga');
+    }
+    public function bukbers()
+    {
+        return $this->belongsToMany(Warga::class, 'bukber_warga');
+    }
+    // public function konsumsibukber()
+    // {
+    //     return $this->belongsToMany(Warga::class, 'konsumsi_warga', 'warga_bukber_id','konsumsi_id');
+    // }
+    
 }

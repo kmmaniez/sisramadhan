4e535d24390978bfe96bf188bc8e6ba12ed3e1b2
kmmaniez <dzlabsid@gmail.com>
2023-06-20 10:28:00 +0700

    update tarawih page

--- resources/views/admin/tarawih/edit.blade.php
+++ resources/views/admin/tarawih/edit.blade.php
@@ -1,19 +1,18 @@
 @php
-    $DateConv = new Hijri_GregorianConvert;
-    $format="YYYY/MM/DD";
+    $DateConv = new Hijri_GregorianConvert();
+    $format = 'YYYY/MM/DD';
     $listTahun = [];
-    for ($i=0; $i < 3; $i++) { 
+    for ($i = 0; $i < 3; $i++) {
         array_push($listTahun, date('Y') - $i);
     }
 @endphp
 @extends('layouts.admin')
 
 @section('content')
-    
     <div class="container">
         <div class="title text-center mb-5">
             <h1>Tambah Data Tarawih</h1>
-            <h2>Tahun {{ date('Y') }}/<?= $DateConv->GregorianToHijri(date('Y'),'YYYY'). 'H'; ?></h2>
+            <h2>Tahun {{ date('Y') }}/<?= $DateConv->GregorianToHijri(date('Y'), 'YYYY') . 'H' ?></h2>
         </div>
         <hr class="mb-5">
         <a href="{{ route('tarawih.index') }}" class="btn btn-lg btn-secondary mb-4">Kembali</a>
@@ -23,17 +22,18 @@
             @method('PUT')
             <div class="form-group mb-3">
                 <label for="tanggal" class="form-label">Tanggal</label>
-                <input type="date" class="form-control" name="tanggal" id="tanggal" value="{{ old('tanggal', $tarawih->tgl_kegiatan) }}">
+                <input type="date" class="form-control" name="tanggal" id="tanggal"
+                    value="{{ old('tanggal', $tarawih->tgl_kegiatan) }}">
             </div>
             <div class="form-group mb-3">
                 <label for="id_imam" class="form-label">Nama Imam</label>
                 <select class="form-select" name="id_imam" id="id_imam">
                     {{-- <option value="" selected style="display: none;">Pilih imam</option> --}}
-                    @foreach ($warga as $key => $data)
-                        @if (old('id_imam', $tarawih->imam->id ) == $data->id)
-                            <option value="{{ $data->id }}" selected>{{ $tarawih->imam->nama_alias }}</option>
+                    @foreach ($usersImam as $key => $value)
+                        @if (old('id_imam', $tarawih->imam->id) == $value->id)
+                            <option value="{{ $value->id }}" selected>{{ $tarawih->imam->nama_alias }}</option>
                         @else
-                            <option value="{{ $data->id }}">{{ $data->nama_alias }}</option>
+                            <option value="{{ $value->id }}">{{ $value->nama_alias }}</option>
                         @endif
                     @endforeach
                 </select>
@@ -42,11 +42,11 @@
                 <label for="id_penceramah" class="form-label">Nama Pengisi Kultum</label>
                 <select class="form-select" name="id_penceramah" id="id_penceramah">
                     {{-- <option value="" selected style="display: none;">Pilih penceramah</option> --}}
-                    @foreach ($warga as $key => $data)
-                        @if (old('id_imam', $tarawih->penceramah->id ) == $data->id)
-                            <option value="{{ $data->id }}" selected>{{ $tarawih->penceramah->nama_alias }}</option>
+                    @foreach ($usersPenceramah as $key => $value)
+                        @if (old('id_penceramah', $tarawih->penceramah->id) == $value->id)
+                            <option value="{{ $value->id }}" selected>{{ $tarawih->penceramah->nama_alias }}</option>
                         @else
-                            <option value="{{ $data->id }}">{{ $data->nama_alias }}</option>
+                            <option value="{{ $value->id }}">{{ $value->nama_alias }}</option>
                         @endif
                     @endforeach
                 </select>
@@ -55,11 +55,11 @@
                 <label for="id_bilal" class="form-label">Nama Bilal</label>
                 <select class="form-select" name="id_bilal" id="id_bilal">
                     {{-- <option value="" selected style="display: none;">Pilih bilal</option> --}}
-                    @foreach ($warga as $key => $data)
-                        @if (old('id_bilal', $tarawih->bilal->id ) == $data->id)
-                            <option value="{{ $data->id }}" selected>{{ $tarawih->bilal->nama_alias }}</option>
+                    @foreach ($usersBilal as $key => $value)
+                        @if (old('id_bilal', $tarawih->bilal->id) == $value->id)
+                            <option value="{{ $value->id }}" selected>{{ $tarawih->bilal->nama_alias }}</option>
                         @else
-                            <option value="{{ $data->id }}">{{ $data->nama_alias }}</option>
+                            <option value="{{ $value->id }}">{{ $value->nama_alias }}</option>
                         @endif
                     @endforeach
                 </select>
@@ -68,11 +68,10 @@
                 <label for="keterangan" class="form-label">Keterangan</label>
                 <textarea name="keterangan" id="keterangan" cols="10" rows="5" class="form-control">{{ old('keterangan', $tarawih->keterangan) }}</textarea>
             </div>
-            
+
             <button type="submit" class="btn btn-lg btn-dark mt-3">Simpan</button>
 
         </form>
 
     </div>
-
 @endsection

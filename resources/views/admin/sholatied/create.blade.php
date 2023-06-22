67f32f709f350a44899d1dc2f0158343096546f3
kmmaniez <dzlabsid@gmail.com>
2023-06-22 14:17:26 +0700

    sholatied final

--- resources/views/admin/sholatied/create.blade.php
+++ resources/views/admin/sholatied/create.blade.php
@@ -1,3 +1,6 @@
+@php
+    $DateConv = new Hijri_GregorianConvert;
+@endphp
 @extends('layouts.admin')
 
 @section('content')
@@ -5,7 +8,7 @@
     <div class="container">
         <div class="title text-center mb-5">
             <h1>Tambah Data Sholat Ied</h1>
-            <h2>Tahun 2022/1443 H</h2>
+            <h2>Tahun  {{ date('Y') }}/<?= $DateConv->GregorianToHijri(date('Y'),'YYYY'). 'H'; ?></h2>
         </div>
         <hr class="mb-4">
         <a href="{{ route('sholatied.index') }}" class="btn btn-lg btn-secondary mb-4">Kembali</a>
@@ -21,15 +24,7 @@
                 <label for="tempat" class="form-label">Tempat Sholat Ied</label>
                 <input type="text" class="form-control" name="tempat" id="tempat">
             </div>
-            {{-- <div class="form-group mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Tempat Sholat</label>
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Pilih nama</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div> --}}
+
             <div class="form-group mb-3">
                 <label for="keterangan" class="form-label">Keterangan</label>
                 <textarea name="keterangan" id="keterangan" cols="10" rows="5" class="form-control">

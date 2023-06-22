da10f3aa737b66031930ca4d16d5b1ee0bd25d6f
kmmaniez <dzlabsid@gmail.com>
2023-06-22 14:18:45 +0700

    M tadarus final

--- app/Models/Tadarus.php
+++ app/Models/Tadarus.php
@@ -12,17 +12,7 @@ class Tadarus extends Model
     protected $guarded = ['id'];
 
     protected $with = ['wargas'];
-    // public $timestamps = false;
 
-    // public function kelompok()
-    // {
-    //     return $this->belongsTo(KelTadarus::class, 'id_kel_tadarus');
-    // }
-
-    // public function warga()
-    // {
-    //     return $this->belongsTo(Warga::class,'id_warga');
-    // }
     public function wargas()
     {
         return $this->belongsToMany(Warga::class,'tadarus_warga');
